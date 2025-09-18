c874469d74fb75b26e3a7e588fd9e02bf0ac787b
nadav <nadav@eazix.io>
2025-09-18 19:04:21 +0300

    back to curser - 25 - general - 12

--- beams/src/app/threejs-box/threejs-box.module.ts
+++ beams/src/app/threejs-box/threejs-box.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ThreejsBoxComponent } from './threejs-box.component';
 import { AngularMaterialModule } from '../angular-material.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
     declarations: [ThreejsBoxComponent],
-    imports: [CommonModule, FormsModule, AngularMaterialModule],
+    imports: [CommonModule, FormsModule, AngularMaterialModule, TranslateModule],
     exports: [ThreejsBoxComponent]
 })
 export class ThreejsBoxModule { }
